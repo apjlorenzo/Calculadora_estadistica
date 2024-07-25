cef9b40a5d01167b4dbd70fe8c597513f9a2ab86
Pablo Lorenzo <apjlorenzo1@gmail.com>
2024-07-25 16:03:02 -0600

    Añadí nuevos metodos

--- Program.cs
+++ Program.cs
@@ -1,16 +1,43 @@
-﻿while(true)
+﻿internal class Program
 {
-    MostrarMenú();
-    int opcion = int.Parse(Console.ReadLine());
-    switch(opcion)
+    static List<double> Numeros = new List<double>();
+    static double Total = 0;
+    static int Cantidad = 0;
+    static double Resultado = 0;
+    //Ciclo para el menú
+    static void Main(string[] args)
     {
-        case 1:
-            PedirNumeros();
-            break;
-        case 2:
-            break;
+        while (true)
+        {
+            MostrarMenú();
+            int opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+                    PedirNumeros();
+                    break;
+                case 2:
+                    Resultado = CalcularMedia(Total, Cantidad);
+                    MostrarResultado("La media calculada es: " + Resultado); 
+                    break;
+                case 3:
+                    MostrarResultado();
+                    break;
+                case 4:
+                    MostrarResultado();
+                    break;
+                case 5:
+                    MostrarResultado();
+                    break;
+                case 6:
+                    PedirNumeros();
+                    break;
+                case 7:
+                    MostrarResultado();
+                    break;
+            }
+        }
     }
-}
 //Método para mostrar menú
 static void MostrarMenú()
 {
@@ -23,6 +50,7 @@ static void MostrarMenú()
     Console.WriteLine("4.Calcular la moda.");
     Console.WriteLine("5.Calcular la desviación estándar.");
     Console.WriteLine("6.Cambiar de números.");
+    Console.WriteLine("7.Mostrar resultados");
     Console.Write("Escriba una opción: ");
 }
 //Método para pedir y guardar numeros
@@ -30,14 +58,43 @@ static void PedirNumeros()
 {
     Console.Clear();
     Console.Write("Ingrese la cantidad de números que quiere ingresar: ");
-    int cantidad = int.Parse(Console.ReadLine());
-    for (int i = 0; i < cantidad; i++)
+    RegistrarNumeros();
+}
+static void RegistrarNumeros()
+{
+    double Entrada = 0;
+    Cantidad = int.Parse(Console.ReadLine());
+    for (int i = 0; i < Cantidad; i++)
     {
-        Console.Write($"Ingrese el {i+1} número: ");
-        double Numero = double.Parse(Console.ReadLine());
+        Console.Write($"Ingrese el {i + 1} número: ");
+            Numeros.Add(double.Parse(Console.ReadLine()));
+            Total += Numeros[i];
     }
     Console.Clear();
     Console.WriteLine("Números registrados...");
     Console.WriteLine("\nPresione una tecla para continuar: ");
     Console.ReadKey();
+}
+    static double CalcularMedia(double a, double b)
+    {
+        return a / b;    
+    }
+    static void MostrarResultado()
+    {
+        Console.Clear();
+        Console.WriteLine("Números ingresados: ");
+        foreach (double numero in Numeros)
+        {
+            Console.WriteLine(numero);
+        }
+        Console.WriteLine("Ultimo resultado: "+ Resultado);
+        Console.ReadKey();
+    }
+    static void MostrarResultado(string mensaje)
+    {
+        Console.Clear();
+        Console.WriteLine(mensaje);
+        Console.ReadKey();
+    }
+
 }
